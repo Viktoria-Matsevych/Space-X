47b16f59873af4cd7f0422893cb4377ef91f3be3
Viktoria-Matsevych <macevicviktoria1@gmail.com>
2025-08-03 16:29:17 +0300

    Зробила годинник до сайту

--- script.js
+++ script.js
@@ -7,11 +7,26 @@ document.addEventListener("DOMContentLoaded", () => {
     setTimeout(() => {
             loader.style.display = "none"; // Приховати лоадер
             container.style.display = "block" // Показати основний контент
+        loader.style.display = "none"; // Приховати лоадер
+        container.style.display = "block" // Показати основний контент
 
     }, 1000);
 })
+const s = document.querySelector(".sec")
+const m = document.querySelector(".min")
+const h = document.querySelector(".hour")
+setInterval(() => {
+    const now = new Date()
+    const sec = now.getSeconds()
+    const min = now.getMinutes()
+    const hs = now.getHours()
 
 let textsUk = [
 Фалькон9, ФальконВажкий, Дракон, Оновлення
 
-];
+    s.style.transform = `rotate(${sec * 6}deg)`
+    m.style.transform = `rotate(${min * 6}deg)`
+    h.style.transform = `rotate(${hs * 30}deg)`
+
+    console.log(sec)
+}, 1000)
